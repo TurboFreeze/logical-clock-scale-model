f5967323a7ac89f4719ff4f72b6dd727a70b9a98
TurboFreeze <iturbofreeze@gmail.com>
2018-03-04 22:45:13 -0500

    Cleaned and documented Controller

--- Controller.py
+++ Controller.py
@@ -3,23 +3,31 @@ from random import randint
 import thread
 
 class Controller():
-    
+
     vms = [None, None, None]
-    
+
     def __init__(self):
+        """
+        Initialization of controller for scale model
+        """
         print "Controller init"
-        # TODO threading?
-        self.vms[0] = Model(randint(1,6), 0, self) # TODO get rid of spaghetti with sockets
-        self.vms[1] = Model(randint(1,6), 1, self)
-        self.vms[2] = Model(randint(1,6), 2, self)
-        
+
+        speed_range = 6
+        # Initializing the virtual machines
+        self.vms[0] = Model(randint(1, speed_range), 0, self)
+        self.vms[1] = Model(randint(1, speed_range), 1, self)
+        self.vms[2] = Model(randint(1, speed_range), 2, self)
+
         # thread 'em out
         thread.start_new_thread(self.vms[0].start, ())
         thread.start_new_thread(self.vms[1].start, ())
         thread.start_new_thread(self.vms[2].start, ())
-    
-    
+
+
     def handle_message(self, vm_index, msg):
-        self.vms[vm_index].messages.append(msg) # TODO re-examine
-    
-    
+        """
+        Pass message on to appropriate receiving machine
+        :param vm_index: ID of receiving machine
+        :param msg: Message being sent
+        """
+        self.vms[vm_index].messages.append(msg)
